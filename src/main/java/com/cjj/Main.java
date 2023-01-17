cc510bb37693fb6af5d09c001e6117ce8b84c96a
cjj <1415466826@qq.com>
2023-01-17 11:09:06 +0800

    first commit

---
+++ src/main/java/com/cjj/Main.java
@@ -0,0 +1,14 @@
+package com.cjj;
+
+/**
+ * Description:
+ *
+ * @Author: CaiJunJie
+ * @Create: ${DATE}-${TIME}
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
